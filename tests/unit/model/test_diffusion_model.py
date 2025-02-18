d5d8c51bea71f440c9ce45c2b21400fedf2f59d0
Coos Baakman <coos.baakman@radboudumc.nl>
2025-02-18 19:45:26 +0000

    overwrite if same time point

--- tests/unit/model/test_diffusion_model.py
+++ tests/unit/model/test_diffusion_model.py
@@ -22,6 +22,14 @@ def test_save_rmsds():
         assert data.shape[0] == 1
         assert data.shape[1] == (n + 1)
 
+        # should overwrite
+        Conditional_Diffusion_Model._save_rmsds(run_id, 0, torch.rand(n), n * ["test"])
+
+        data = pandas.read_csv(table_path)
+
+        assert data.shape[0] == 1
+        assert data.shape[1] == (n + 1)
+
     finally:
         if os.path.isfile(table_path):
             os.remove(table_path)
